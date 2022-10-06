2b17d9afcecfc626b9fae298279e8cd00070a336
Igor Jovicic <87913675+R0rschach02@users.noreply.github.com>
2022-10-06 09:26:21 +0200

    Übung 2 aufgabe 1

---
+++ QuelleNew.c
@@ -0,0 +1,30 @@
+/****************************************************************
+* Dateiname: QuelleUebung2.c
+* Beschreibung: IT1 Übungsblatt 2 Aufgabe 1
+* Autor: Igor Jovicic
+* Datum: 06.10.2022
+* Version: 1.0
+*****************************************************************/
+
+
+#include <stdio.h>	// Standard Eingabe/Ausgabe Funktionen
+
+int main(void)
+{
+	int zeichen;
+	int einruecken;
+
+	for (zeichen = 0; zeichen <= 2; zeichen++)
+	{
+		printf(" test \t \n");
+
+		if ( zeichen > 1 )
+			{
+				printf(" \t");
+			}
+		
+	}
+
+	return 0;
+}
+
